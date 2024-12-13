1fe1a40af4dad109d0d13270c83a23ebf2705307
iamheesoo <beforeheesoo@gmail.com>
2024-12-13 16:32:39 +0900

    [Add] LandingPage 추가

---
+++ lib/landingpage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Image.asset('assets/image/landing.png', fit: BoxFit.cover),
+      ),
+    );
+  }
+}
