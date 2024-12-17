8f9a9a666ceef9a27bb162f98e6aee47d556963d
iamheesoo <beforeheesoo@gmail.com>
2024-12-18 08:31:11 +0900

    [Update] PageView 적용

--- lib/cards/postcard.dart
+++ lib/cards/postcard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class PostCard extends StatefulWidget {
   int number;
@@ -10,6 +11,15 @@ class PostCard extends StatefulWidget {
 }
 
 class _PostCardState extends State<PostCard> {
+  int pageSize = 3;
+  int currentPageIndex = 0;
+  PageController _controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,36 +47,64 @@ class _PostCardState extends State<PostCard> {
               ],
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 400,
-            color: Colors.white,
-            child: Image.network(
-              "https://picsum.photos/id/${widget.number}/200/300",
-              fit: BoxFit.fitWidth,
+          AspectRatio(
+            aspectRatio: 0.8,
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                PageView.builder(
+                    controller: _controller,
+                    itemCount: pageSize,
+                    onPageChanged: (value) {
+                      setState(() {
+                        currentPageIndex = value;
+                      });
+                    },
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 400,
+                        color: Colors.white,
+                        child: Image.network(
+                          "https://picsum.photos/id/${index + 300}/200/300",
+                          fit: BoxFit.fitWidth,
+                        ),
+                      );
+                    }),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5,
+                    horizontal: 5,
+                  ),
+                  margin: EdgeInsets.all(10),
+                  child: Text("${currentPageIndex+1} / $pageSize"),
+                  decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(0.5),
+                      borderRadius: BorderRadius.circular(500)),
+                )
+              ],
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 50,
-            padding: EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.favorite_border_rounded),
-                    SizedBox(width: 10),
-                    Icon(Icons.comment_outlined),
-                    SizedBox(width: 10),
-                    Icon(Icons.send_rounded),
-                  ],
-                ),
-                Text("indicator"),
-                Icon(Icons.bookmark_border_outlined)
-              ],
-            ) 
-            
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.favorite_border_rounded),
+                      SizedBox(width: 10),
+                      Icon(Icons.comment_outlined),
+                      SizedBox(width: 10),
+                      Icon(Icons.send_rounded),
+                    ],
+                  ),
+                  Icon(Icons.bookmark_border_outlined)
+                ],
+              ),
+              SmoothPageIndicator(controller: _controller, count: pageSize),
+            ],
           ),
           Container(
             width: MediaQuery.of(context).size.width,
